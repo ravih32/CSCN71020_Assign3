3d6720d4f10762d98bab18deba6b7f20b5c8c708
Ravi Heera <rheera17@gmail..com>
2025-11-08 12:28:03 -0500

    feat(green): implement minimal Draw logic to pass SameMove_YieldsDraw

--- Assignment3/rps.c
+++ Assignment3/rps.c
@@ -1,6 +1,11 @@
 #include "rps.h"
+#include <string.h>
 
 const char* rps_winner(const char* p1, const char* p2) {
-    // Stub: always wrong for now ? guarantees a test failure (RED)
-    return "Invalid";
+    
+    if (p1 && p2 && strcmp(p1, p2) == 0) {
+        return "Draw";
+    }
+    return "Invalid"; // placeholder for now
 }
+
