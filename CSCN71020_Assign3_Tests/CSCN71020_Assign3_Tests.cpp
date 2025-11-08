381f3055e4d19f99817303467769695f1001004a
Ravi Heera <rheera17@gmail..com>
2025-11-08 11:46:39 -0500

    bootstrap solution with Assignment3 and CSCN71020_Assign3_Tests

---
+++ CSCN71020_Assign3_Tests/CSCN71020_Assign3_Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CSCN71020Assign3Tests
+{
+	TEST_CLASS(CSCN71020Assign3Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
